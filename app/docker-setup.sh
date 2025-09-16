ccb7aebd29d9698fcf6ec2b09b5bcefdc6bfbc95
Subrata Kumar De <subrata.de@pasdgtal.com>
2025-09-16 15:24:22 +0530

    add: docker installation script

--- app/docker-setup.sh
+++ app/docker-setup.sh
@@ -1,25 +1,13 @@
-#!/bin/bash
+# Install docker
+
 
 source <(curl -s https://raw.githubusercontent.com/subrata-pasdt/scripts/main/common/pasdt-devops-scripts.sh)
+
 show_header "Docker Setup" "Script to install and setup docker" "16-Sept-2025" "1.0.0"
 
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-
-# Install the docker-ce
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt update
+sudo apt install -y docker.io docker-compose
 
 # Create docker group
 sudo groupadd docker
@@ -35,4 +23,3 @@ sudo systemctl restart docker.service
 
 # Reload group permissions
 newgrp $USER
-
